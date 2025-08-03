b162a8855d6a67821569dc98828818b27431f28c
Aung2024 <167290626+Aung2024@users.noreply.github.com>
2025-08-03 16:01:28 +0630

    firebase set up

---
+++ js/firebase.js
@@ -0,0 +1,15 @@
+
+// js/firebase.js
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC8-y2xnLINlVTWOOaU8-w82RBzSo2djAQ",
+  authDomain: "labourcare-2481a.firebaseapp.com",
+  projectId: "labourcare-2481a",
+  storageBucket: "labourcare-2481a.appspot.com",
+  messagingSenderId: "1033457212744",
+  appId: "1:1033457212744:web:4d767eb4ef246b1090e77d"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
