dd2a093003ca6d72b3d677b62768155ff518ec16
Aung2024 <167290626+Aung2024@users.noreply.github.com>
2026-07-09 10:21:25 +0630

    newborn

--- js/facility-config.js
+++ js/facility-config.js
@@ -10,7 +10,25 @@
     { code: '001', name_en: 'NPW 500 BED', name_mm: 'NPW 500 BED' },
     { code: '002', name_en: 'MNMA SDG', name_mm: 'MNMA SDG' },
     { code: '003', name_en: 'Other', name_mm: 'Other' },
-    { code: '004', name_en: 'MNCWA Maternity Homes', name_mm: 'MNCWA Maternity Homes' }
+    { code: '004', name_en: 'MNCWA Maternity Homes', name_mm: 'MNCWA Maternity Homes' },
+    { code: '005', name_en: 'Nay Pyi Taw Public Health Department', name_mm: 'နေပြည်တော် ပြည်သူ့ကျန်းမာရေးဦးစီးဌာန' },
+    { code: '006', name_en: 'Pyinmana Township Public Health Department', name_mm: 'ပျဉ်းမနားမြို့နယ်ပြည်သူ့ကျန်းမာရေးဉီးစီးဌာန' },
+    { code: '007', name_en: 'Pyinmana General Hospital (200 Bedded)', name_mm: 'Pyinmana General Hospital (200 Bedded)' },
+    { code: '008', name_en: 'Maternal and Child Health Center (MCH)', name_mm: 'Maternal and Child Health Center (MCH)' },
+    { code: '009', name_en: 'Ywar Kawk (East) SRHC', name_mm: 'Ywar Kawk (East) SRHC' },
+    { code: '010', name_en: 'Paung Laung SRHC', name_mm: 'Paung Laung SRHC' },
+    { code: '011', name_en: 'Ywar Kawk (West) SRHC', name_mm: 'Ywar Kawk (West) SRHC' },
+    { code: '012', name_en: 'U Yin Su SRHC', name_mm: 'U Yin Su SRHC' },
+    { code: '013', name_en: 'Nat Tha Ye RHC', name_mm: 'Nat Tha Ye RHC' },
+    { code: '014', name_en: 'Zee Hpyu Pin SRHC', name_mm: 'Zee Hpyu Pin SRHC' },
+    { code: '015', name_en: 'Naung Pin Thar SRHC', name_mm: 'Naung Pin Thar SRHC' },
+    { code: '016', name_en: 'Sin Thay SRHC', name_mm: 'Sin Thay SRHC' },
+    { code: '017', name_en: 'Kin Mun Tan SRHC', name_mm: 'Kin Mun Tan SRHC' },
+    { code: '018', name_en: 'Zee Kone RHC', name_mm: 'Zee Kone RHC' },
+    { code: '019', name_en: 'Thit Lay Lone SRHC', name_mm: 'Thit Lay Lone SRHC' },
+    { code: '020', name_en: 'Myauk Lut Kone SRHC', name_mm: 'Myauk Lut Kone SRHC' },
+    { code: '021', name_en: 'Pyu Twin SRHC', name_mm: 'Pyu Twin SRHC' },
+    { code: '022', name_en: 'Taung Thar SRHC', name_mm: 'Taung Thar SRHC' }
   ];
 
   function getFacilities() {
